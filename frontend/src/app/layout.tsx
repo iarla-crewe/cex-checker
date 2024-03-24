1e4050d5e366ab38d7bb89dc957a83c9f3c2c054
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2024-03-24 12:06:16 +0100

    Add favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "CEX Checker | Quartz",
   description: "Compare CEXs for the best price",
+  icons: {
+    icon: '/favicon.ico'
+  }
 };
 
 export default function RootLayout({
