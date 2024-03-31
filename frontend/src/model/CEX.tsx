5e783cbe808c4db19b1515d62b26ea8359550458
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2024-03-31 16:01:53 +0200

    Add ByBit, Coinbase, Crypto.com exchanges to list

--- frontend/src/model/CEX.tsx
+++ frontend/src/model/CEX.tsx
@@ -7,5 +7,8 @@ interface CEX {
 
 export const CEXs: CEX[] = [
     {name: "Binance", logoSrc: "/binance.svg", brandColor: "#F1B90C", textColor: "black"},
-    {name: "Kraken", logoSrc: "/kraken.svg", brandColor: "#7132F5", textColor: "white"}
+    {name: "Kraken", logoSrc: "/kraken.svg", brandColor: "#7132F5", textColor: "white"},
+    {name: "ByBit", logoSrc: "/bybit.svg", brandColor: "#17181e", textColor: "white"},
+    {name: "Coinbase", logoSrc: "/coinbase.svg", brandColor: "#004af7", textColor: "white"},
+    {name: "Crypto.com", logoSrc: "/crypto-com.svg", brandColor: "#032f69", textColor: "white"}
 ]
