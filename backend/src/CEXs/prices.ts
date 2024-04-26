14880cdce88243559d634e7fa970f496916f4c34
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2024-04-27 00:55:33 +0100

    Add maker taker fees

--- backend/src/CEXs/prices.ts
+++ backend/src/CEXs/prices.ts
@@ -3,7 +3,7 @@ import { ExchangeFees } from "../types.js";
 export let exchangeTakerFees: ExchangeFees = {
     binance: 0.001,
     kraken: 0.004,
-    coinbase: 0,
+    coinbase: 0.006,
     crypto_com: 0.00075,
     bybit: 0.001
 }
@@ -11,7 +11,7 @@ export let exchangeTakerFees: ExchangeFees = {
 export let exchangeMakerFees: ExchangeFees = {
     binance: 0.001,
     kraken: 0.0024,
-    coinbase: 0,
+    coinbase: 0.004,
     crypto_com: 0.00075,
     bybit: 0.001
 }
