6f1fac724cac37d7689cfcd520d1440f33ddf001
Nelo0 <83479019+Nelo0@users.noreply.github.com>
2024-05-15 02:20:26 +0100

    Style dropdown currency menu

--- frontend/src/components/TradeInfo/SelectCurrency.tsx
+++ frontend/src/components/TradeInfo/SelectCurrency.tsx
@@ -1,5 +1,4 @@
 import styles from "./TradeInfo.module.css"
-import { Button } from "@/components/ui/button"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -32,12 +31,12 @@ export default function SelectCurrency(props: SelectCurrencyProps) {
                     <p>{selected.toUpperCase()}</p>
                 </button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
+            <DropdownMenuContent className={styles["DropdownMenuContent"]}>
                 <DropdownMenuLabel>Choose Token</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuRadioGroup value={selected} onValueChange={onClickHandler}>
                     {currencies.map((currency, index) => (
-                        <DropdownMenuRadioItem key={index} value={currency}>{currency.toUpperCase()}</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem key={index} value={currency} className={styles["DropdownMenuRadioItem"]}>{currency.toUpperCase()}</DropdownMenuRadioItem>
                     ))}
                 </DropdownMenuRadioGroup>
             </DropdownMenuContent>
