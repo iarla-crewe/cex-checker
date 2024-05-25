c85de6fa8b9667841132cd810f16450e400f4f71
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2024-05-25 14:13:45 +0200

    Change page title

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CEX Checker | Quartz",
+  title: "Exchange Scout | Quartz",
   description: "Compare CEXs for the best price and fees",
   icons: {
     icon: "/favicon.ico"
