d4341a45a11fb09fc498e09a36abd33ee24ae63b
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2024-06-06 17:51:15 +0200

    Fade working

--- frontend/src/components/SelectFilter.tsx
+++ frontend/src/components/SelectFilter.tsx
@@ -2,7 +2,7 @@ import { UpdatePriceQuery } from "@/model/API";
 import styles from "./SelectFilter.module.css";
 import { FilterObj, FilterOptionValue } from "@/model/FilterData";
 import FilterOption from "./FilterOption";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { usePostHog } from 'posthog-js/react'
 
@@ -13,7 +13,38 @@ interface SelectFilterProps {
 
 export default function SelectFilter(props: SelectFilterProps) {
     const { handleUpdate, filter } = props;
-    const posthog = usePostHog()
+    const posthog = usePostHog();
+    const scrollableWrapperRef = useRef<HTMLDivElement>(null);
+    const scrollableContentRef = useRef<HTMLDivElement>(null);
+
+    const updateFadeSizes = () => {
+        const wrapper = scrollableWrapperRef.current;
+        const scrollable = scrollableContentRef.current;
+        if (!wrapper || !scrollable) return;
+
+        if (scrollable.scrollWidth <= wrapper.clientWidth) {
+            document.documentElement.setAttribute('filter-fade', "none");
+            return;
+        }
+        
+        if (wrapper.scrollLeft === 0) {
+            document.documentElement.setAttribute('filter-fade', "start");
+        } 
+        else if (wrapper.scrollLeft + wrapper.clientWidth + 1 < scrollable.scrollWidth) {
+            document.documentElement.setAttribute('filter-fade', "middle");
+        }
+    };
+
+    useEffect(() => {
+        updateFadeSizes();
+        const wrapper = scrollableWrapperRef.current;
+
+        if (wrapper) wrapper.addEventListener('scroll', updateFadeSizes);
+
+        return () => {
+            if (wrapper) wrapper.removeEventListener('scroll', updateFadeSizes);
+        };
+    }, []);
 
     const updateFilter = (value: FilterOptionValue) => {
         const index = filter.findIndex(([val]) => val === value[0]);
@@ -33,8 +64,8 @@ export default function SelectFilter(props: SelectFilterProps) {
     }
 
     return (
-        <div className={styles["select-filter-wrapper"]}>
-            <div className={styles["select-filter-content"]}>
+        <div className={styles["select-filter-wrapper"]} ref={scrollableWrapperRef}>
+            <div className={styles["select-filter-content"]} ref={scrollableContentRef}>
                 <Image
                     src="/filter_icon.svg"
                     alt="CEX Filter"
@@ -51,7 +82,6 @@ export default function SelectFilter(props: SelectFilterProps) {
                     />
                 ))}
             </div>
-            <div className={styles["fade-mask"]}/>
         </div>
     );
 }
