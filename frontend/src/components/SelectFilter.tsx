8dfc97ab61c34d0e0e860502aa0639c5375faa56
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2024-06-07 17:52:06 +0200

    Style buttons for manually scrolling

--- frontend/src/components/SelectFilter.tsx
+++ frontend/src/components/SelectFilter.tsx
@@ -8,7 +8,15 @@ import { usePostHog } from 'posthog-js/react'
 
 interface SelectFilterProps {
     handleUpdate: (data: UpdatePriceQuery) => void;
-    filter: FilterOptionValue[]
+    filter: FilterOptionValue[];
+}
+
+interface ScrollArrowProps {
+    src: string;
+    alt: string;
+    direction: string;
+    onClick: () => void;
+    enabled: boolean;
 }
 
 export default function SelectFilter(props: SelectFilterProps) {
@@ -17,6 +25,9 @@ export default function SelectFilter(props: SelectFilterProps) {
     const scrollableWrapperRef = useRef<HTMLDivElement>(null);
     const scrollableContentRef = useRef<HTMLDivElement>(null);
 
+    const [leftScroll, setLeftScroll] = useState(false);
+    const [rightScroll, setRightScroll] = useState(true);
+
     const updateFadeSizes = () => {
         const wrapper = scrollableWrapperRef.current;
         const scrollable = scrollableContentRef.current;
@@ -24,14 +35,21 @@ export default function SelectFilter(props: SelectFilterProps) {
 
         if (scrollable.scrollWidth <= wrapper.clientWidth) {
             document.documentElement.setAttribute('filter-fade', "none");
-            return;
+            setLeftScroll(false);
+            setRightScroll(false);
         }
-        
-        if (wrapper.scrollLeft === 0) {
+        else if (wrapper.scrollLeft === 0) {
             document.documentElement.setAttribute('filter-fade', "start");
+            setLeftScroll(false);
+            setRightScroll(true);
         } 
         else if (wrapper.scrollLeft + wrapper.clientWidth + 1 < scrollable.scrollWidth) {
             document.documentElement.setAttribute('filter-fade', "middle");
+            setLeftScroll(true);
+            setRightScroll(true);
+        } else {
+            setLeftScroll(true);
+            setRightScroll(false);
         }
     };
 
@@ -49,6 +67,7 @@ export default function SelectFilter(props: SelectFilterProps) {
     const manuallyScroll = (amount: number) => {
         const wrapper = scrollableWrapperRef.current;
         if (wrapper) wrapper.scrollLeft += amount;
+        updateFadeSizes();
     }
 
     const updateFilter = (value: FilterOptionValue) => {
@@ -69,52 +88,50 @@ export default function SelectFilter(props: SelectFilterProps) {
     }
 
     return (
-        <div className={styles["select-filter-wrapper"]} ref={scrollableWrapperRef}>
-            <button
-                className={styles["scroll-button"]}
-                id="left"
-                onClick={() => {manuallyScroll(-100)}}
-            >
-                <Image
-                    src="/left_arrow.svg"
-                    alt="Scroll Left"
-                    height={30}
-                    width={30}
-                    className={styles["scroll-icon"]}
-                />
-            </button>
+        <div className={styles["select-filter-wrapper"]}>
+            <ScrollArrow src="/left_arrow.svg" alt="Scroll Left" direction="s-left" onClick={() => {manuallyScroll(-100)}} enabled={leftScroll}/>
+            <ScrollArrow src="/right_arrow.svg" alt="Scroll Right" direction="s-right" onClick={() => {manuallyScroll(100)}} enabled={rightScroll}/>
 
-            <button
-                className={styles["scroll-button"]}
-                id="right"
-                onClick={() => {manuallyScroll(100)}}
-            >
-                <Image
-                    src="/right_arrow.svg"
-                    alt="Scroll Right"
-                    height={30}
-                    width={30}
-                    className={styles["scroll-icon"]}
-                />
-            </button>
-            
-            <div className={styles["select-filter-content"]} ref={scrollableContentRef}>
-                <Image
-                    src="/filter_icon.svg"
-                    alt="CEX Filter"
-                    height={30}
-                    width={30}
-                    className={styles["filter-icon"]}
-                />
-                {filter.map(([cex, enabled], _) => (
-                    <FilterOption 
-                        key={cex}
-                        name={cex} 
-                        defaultEnabled={enabled}
-                        onUpdate={(value: boolean) => updateFilter([cex, value])}
+            <div className={styles["select-filter-container"]} ref={scrollableWrapperRef}>
+                <div className={styles["select-filter-content"]} ref={scrollableContentRef}>
+                    <Image
+                        src="/filter_icon.svg"
+                        alt="CEX Filter"
+                        height={30}
+                        width={30}
+                        className={styles["filter-icon"]}
                     />
-                ))}
+                    {filter.map(([cex, enabled], _) => (
+                        <FilterOption 
+                            key={cex}
+                            name={cex} 
+                            defaultEnabled={enabled}
+                            onUpdate={(value: boolean) => updateFilter([cex, value])}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
+}
+
+function ScrollArrow(props: ScrollArrowProps) {
+    const { src, alt, direction, onClick, enabled } = props;
+
+    if (!enabled) return (<></>)
+
+    return (
+        <button
+            className={styles[direction]}
+            onClick={onClick}
+        >
+            <Image
+                src={src}
+                alt={alt}
+                height={25}
+                width={25}
+                className={styles["scroll-icon"]}
+            />
+        </button>
+    )
 }
